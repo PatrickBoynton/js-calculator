4c869b791bc0459de8ac8b5af65e566287935a76
PatrickBoynton <jpboynton3@gmail.com>
2021-01-14 14:32:13 -0500

    Added the query selectors.

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+// First Row
+const calculator = document.querySelector(".calculator");
+
+const calculatorScreen = document.querySelector("input");
+const clear = document.querySelector(".clear");
+const plusMinus = document.querySelector(".plus-minus");
+const percent = document.querySelector(".percent");
+const divide = document.querySelector("[value='/']");
+
+// Second Row
+const seven = document.querySelector("[value='7']");
+const eight = document.querySelector("[value='8']");
+const nine = document.querySelector("[value='9']");
+const times = document.querySelector("[value='*']");
+
+//Third Row
+const four = document.querySelector("[value='4']");
+const five = document.querySelector("[value='5']");
+const six = document.querySelector("[value='6']");
+const minus = document.querySelector("[value='-']");
+
+//Fourth Row
+const one = document.querySelector("[value='1']");
+const two = document.querySelector("[value='2']");
+const three = document.querySelector("[value='3']");
+const plus = document.querySelector("[value='+']");
+
+//Fifth Row
+const zero = document.querySelector(".btn-lg");
+const decimal = document.querySelector(".decimal");
+const equals = document.querySelector(".equal-sign");
